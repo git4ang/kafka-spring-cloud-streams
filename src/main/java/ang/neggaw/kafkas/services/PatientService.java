d05b8edbb2f11023d8500f5b6ca658a6f8067df3
git4ang <code4ang@gmail.com>
2021-08-29 15:21:38 +0200

    Implementation of spring-cloud-kafka-supplier (services package)

--- src/main/java/ang/neggaw/kafkas/services/PatientService.java
+++ src/main/java/ang/neggaw/kafkas/services/PatientService.java
@@ -7,7 +7,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * author by: ANG
@@ -20,15 +22,32 @@ import java.util.function.Consumer;
 public class PatientService {
 
     private final PatientRepository patientRepository;
+    private long idPatientSupplier = 0;
 
     @Bean
     public Consumer<Patient> patientConsumer() {
 
         return (patient) -> {
-            patientRepository.save(patient);
+//            patientRepository.save(patient);
             System.out.println("******************************");
             System.out.println(patient);
             System.out.println("******************************");
         };
+    }
+
+    @Bean
+    public Supplier<Patient> patientSupplier() {
+        return () -> Patient
+                .builder()
+                .idPatient(idPatientSupplier++)
+                .name("Patient-" + idPatientSupplier)
+                .appointment(newRandom(1, 31) + "/" + newRandom(1, 12) + "/2022 " + newRandom(8, 11) + ":" + newRandom(0, 59))
+                .appointmentDuration((7 + new Random().nextInt(59)) + " min")
+                .doctorName("Dr-" + (7 + new Random().nextInt(17)))
+                .build();
+    }
+
+    private int newRandom(int first, int end) {
+        return first + new Random().nextInt(end);
     }
 }
